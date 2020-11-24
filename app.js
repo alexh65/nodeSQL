167c066465ffa6605e695a76a06dbc57cacca5f9
Alex Hoang <nhuhoang1012@gmail.com>
2020-11-23 19:02:33 -0800

    Update port

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ db.connect((err) => {
   }
   console.log('MySQL connected....')
 });
+const port = process.env.PORT || 3000;
 const app = express();
 // Parsing body
 app.use(express.json());
@@ -84,7 +85,7 @@ app.post('/editRectangle', (req, res) => {
     res.redirect('/');
   });
 });
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log("Server started on port 3000");
 });
 
